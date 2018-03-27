6eab77f7434cbc0aa20c5aa7bb4df8dd0a9bc44e
Bartek <bartekwysocki46@gmail.com>
2018-03-27 22:40:31 +0200

    added basic

---
+++ Zabawki/ConsoleApplication1/PokojZabawek.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class PokojZabawek
+    {
+        List<Zabawka> lista = new List<Zabawka>(); 
+
+
+
+        public void add(Zabawka zabawka)
+        {
+            lista.Add(zabawka);
+        }
+
+        public void zmienSzybkosc()
+        {
+            foreach(Zabawka zabawka in lista)
+            {
+                zabawka.Szybkosc+=2;
+            }
+        }
+
+        public void zmienWysokosc()
+        {
+            foreach (Zabawka zabawka in lista)
+            {
+            }
+
+        }
+
+        public void zmienGlebokosc()
+        {
+            foreach (Zabawka zabawka in lista)
+            {
+                zabawka.Glebokosc++;
+
+            }
+        }
+
+
+        public void wyswietl()
+        {
+            foreach (Zabawka zabawka in lista)
+            {
+                Console.WriteLine("Szybkosc zabawki " + zabawka.Szybkosc);
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Zabawki/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wartosc wartosc1 = new Wartosc(23, 300);
+            Zabawka zabawka = new Zabawka(1, 2, 3,wartosc1,20);
+            PokojZabawek pokojZabawek = new PokojZabawek();
+            pokojZabawek.add(zabawka);
+            pokojZabawek.wyswietl();
+            pokojZabawek.zmienSzybkosc();
+            pokojZabawek.wyswietl();
+            Console.WriteLine(zabawka.WartoscAktualna);
+            Console.ReadLine();
+        }
+    }
+}
