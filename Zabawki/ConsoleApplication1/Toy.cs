96303075c02a4a59dc866c36fe9d67ce3fdc7da9
Bartek <bartekwysocki46@gmail.com>
2018-03-28 18:55:38 +0200

    Fixed and working

---
+++ Zabawki/ConsoleApplication1/Toy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    struct Value
+    {
+        private double price;
+        private double sentimentalValue;
+
+
+        public Value(double price, double sentimentalValue)
+        {
+            this.price = price;
+            this.sentimentalValue = sentimentalValue;
+        }
+
+        public double Price {
+            get {
+                return this.price;
+                }
+            set {
+                if(value > 0)
+                {
+                    this.price = value;
+                }else
+                {
+                    Console.WriteLine("Price must be greater than 0");
+                    Console.ReadLine();
+                }
+            }
+        }
+        public double SentimentalValue {
+            get
+            {
+                return this.sentimentalValue;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.sentimentalValue = value;
+                }
+                else
+                {
+                    Console.WriteLine("SentimentalValue must be greater than 0");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    }
+
+
+
+    class Toy
+    {
+        private int speed;
+        private int height;
+        private int depth;
+        private double age;
+
+        Value basicValue;
+
+        public int Speed { get => speed; set => speed = value; }
+        public int Height { get => height; set => height = value; }
+        public int Depth { get => depth; set => depth = value; }
+        public double Age { get => age; set => age = value; }
+
+
+        public Toy(int speed,int height,int depth,double age,Value basicValue)
+        {
+            this.speed = speed;
+            this.height = height;
+            this.depth = depth;
+            this.age = age;
+            this.basicValue = basicValue;
+        }
+
+        public double GetActualValue
+        {
+            get
+            {
+             return basicValue.Price * this.age + basicValue.SentimentalValue;
+            }
+        }
+
+    }
+}
---
+++ Zabawki/ConsoleApplication1/ToysRoom.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ToysRoom
+    {
+        public List<Toy> list = new List<Toy>();
+        public delegate void NumberToyHandler();
+        public event NumberToyHandler ChangeNumber;
+        private double limit;
+        private double sum = 0;
+
+        public double Limit { get => limit; set => limit = value; }
+
+        public delegate void ValueSpeedToyHandler();
+        public event ValueSpeedToyHandler ValueSpeedChange;
+
+        public delegate void ValueHeightToyHandler();
+        public event ValueHeightToyHandler ValueHeightChange;
+
+
+        public delegate void ValueDepthToyHandler();
+        public event ValueDepthToyHandler ValueDepthChange;
+
+
+        public delegate void LimitValueToyHandler();
+        public event LimitValueToyHandler LimitChange;
+
+
+
+
+        public virtual void OnLimitChange()
+        {
+            if (LimitChange != null)
+            {
+                LimitChange();
+            }
+            else
+            {
+                Console.WriteLine("Limit "+ limit + " for actualValue reached!");
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public virtual void OnValueDepthChanged()
+        {
+            if (ValueDepthChange != null)
+            {
+                ValueDepthChange();
+            }
+            else
+            {
+                Console.WriteLine("New Value for depth for Toys!!");
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public virtual void OnValueHeightChanged()
+        {
+            if (ValueHeightChange != null)
+            {
+                ValueHeightChange();
+            }
+            else
+            {
+                Console.WriteLine("New Value for height for Toys!!");
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public virtual void OnValueSpeedChanged()
+        {
+            if(ValueSpeedChange != null)
+            {
+                ValueSpeedChange();
+            }else
+            {
+                Console.WriteLine("New Value for speed for Toys!!");
+                Console.ReadLine();
+            }
+        }
+
+
+
+        public virtual void OnNumberChanged()
+        {
+            if(ChangeNumber != null)
+            {
+                ChangeNumber();
+            }else
+            {
+                Console.WriteLine("New Toy Added!!");
+                Console.ReadLine();
+            }
+        }
+
+
+        public void add(Toy toy)
+        {
+            list.Add(toy);
+            OnNumberChanged();
+
+            foreach(Toy atoy in list)
+            { 
+                sum += atoy.GetActualValue;
+                if(limit < sum)
+                {
+                    OnLimitChange();
+                }
+            }
+        }
+
+        public void changeSpeedToAll(int speed)
+        {
+            foreach(Toy toy in list)
+            {
+                toy.Speed += speed;
+            }
+            OnValueSpeedChanged();
+        }
+
+        public void changeDepthToAll(int depth)
+        {
+            foreach (Toy toy in list)
+            {
+                toy.Depth += depth;
+            }
+            OnValueDepthChanged();
+        }
+
+
+        public void changeHeightToAll(int height)
+        {
+            foreach (Toy toy in list)
+            {
+                toy.Height += height;
+            }
+            OnValueHeightChanged();
+        }
+
+        public void showAllInfo()
+        {
+            System.Console.WriteLine("Number of toys " + list.Count);
+            System.Console.WriteLine("-----------------");
+            int i =1;
+            foreach(Toy toy in list)
+            {
+                System.Console.WriteLine("Toy number: " + i);
+                System.Console.WriteLine("-----------------");
+                System.Console.WriteLine("Toy speed: " + toy.Speed);
+                System.Console.WriteLine("Toy height: " + toy.Height);
+                System.Console.WriteLine("Toy depth: " + toy.Depth);
+                System.Console.WriteLine("Toy age: " + toy.Age);
+                System.Console.WriteLine("Actual value: " + toy.GetActualValue);
+                System.Console.WriteLine("-----------------");
+                i++;
+            }
+
+
+            System.Console.ReadLine();
+        }
+
+
+
+    }
+}
